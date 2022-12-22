03812b45d9b1aff85880b77bc2f6843a6c5cfc73
iloveknuba <xxxcccer982@gmail.com>
2022-12-22 14:12:03 +0200

    Done

--- arrays_project/Form1.Designer.cs
+++ arrays_project/Form1.Designer.cs
@@ -41,16 +41,16 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.Aqua;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(28, 46);
+            this.dataGridView1.Location = new System.Drawing.Point(30, 46);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowHeadersWidth = 32;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(418, 316);
+            this.dataGridView1.Size = new System.Drawing.Size(500, 300);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(577, 339);
+            this.button1.Location = new System.Drawing.Point(577, 342);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
