807654467adf1fd8f70c4700d92271ce0c156ae8
iloveknuba <xxxcccer982@gmail.com>
2022-12-22 13:55:33 +0200

    Добавьте файлы проекта.

---
+++ arrays_project/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace arrays_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Aqua;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(418, 316);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(577, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(577, 195);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(550, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "n=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(550, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "m=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ arrays_project/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrays_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = 0, m = 0; int[,] a;
+            Random ran = new Random();
+            try
+            {
+                n = Convert.ToInt16(textBox1.Text);
+                m = Convert.ToInt16(textBox2.Text);
+                a = new int[n, m];
+                Arrays2.CreateArr(a);
+                Arrays2.PrintAr1(a, dataGridView1);
+
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+        class Arrays2
+        {
+            public bool error = false;
+            int[,] a;
+            int length1;
+            int length2;
+            public Arrays2(int size1, int size2)
+            { a = new int[size1, size2]; length1 = size1; length2 = size2; }
+            public int Length1
+            { get { return length1; } }
+            public int Length2
+            { get { return length2; } }
+
+            private static Random ran = new Random();
+            public static void CreateArr(int[,] a) // creating array
+            {
+                for (int i = 0; i < a.GetLength(0); i++)
+                    for (int j = 0; j < a.GetLength(1); j++)
+
+                        a[i, j] = ran.Next(-10, 10);
+
+            }
+            public static void PrintAr1(int[,] a, DataGridView dg) // printing array in datagridview
+            {
+                int g = 1;
+                DataGridViewTextBoxColumn dgvAge;
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    dgvAge = new DataGridViewTextBoxColumn();
+                    dgvAge.Width = 35;
+                    dg.Columns.Add(dgvAge);
+                }
+                dg.Rows.Clear();
+                dg.RowCount = a.GetLength(0);
+                dg.ColumnCount = a.GetLength(1);
+                for (int i = 0; i < a.GetLength(0); i++)
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        dg.Rows[i].Cells[j].Value = a[i, j].ToString();
+                        if (a[i, j] > 0)
+                            dg.Rows[i].Cells[j].Style.BackColor = Color.Red;  // changing color in cells where elements > 0
+
+                    }
+
+
+            }
+
+
+            public int this[int i, int j]
+            {
+                get
+                {
+                    if ((i >= 0 && i < length1) && (j >= 0 && j <= length2))
+                        return a[i, j];
+                    else
+                    {
+                        error = true;
+                        return 0;
+                    }
+                }
+                set
+                {
+                    if (i >= 0 && i < length1 && j >= 0 && j <= length2 && value >= -10 && value <= 10)
+                        a[i, j] = value;
+                    else error = true;
+                }
+            }
+        }
+    }
+}
